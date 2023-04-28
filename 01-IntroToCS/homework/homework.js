9491a2fcbebc23b09ee01d03f5a9e78b0ea467a6
“rcosme85” <rcosme85@hotmail.com>
2023-04-27 20:39:19 -0500

    Prueba 27Abr

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,33 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+  let resultado = 0
+  let ind = 0
+  for (let i = num.length - 1; i >= 0; i--){
+    resultado = resultado + (2 ** i * num[ind])
+    ind++
+  }
+  console.log(resultado)
+  return(resultado)
+}
 
-function DecimalABinario(num) {}
+function DecimalABinario(num) {
+  let resto = 0;
+  let arr = [];
+  let div = 0;
+  let rpta = "";
+  
+  while (num > 0) {
+    resto = num % 2;
+    div = Math.floor(num / 2);
+    arr.push(resto);
+    num = div;
+  }
+  arr.reverse();
+  rpta = arr.join('');
+//console.log(rpta);
+  return rpta;
+}
 
 module.exports = {
    BinarioADecimal,
--- 04-EstructuraDeDatos-I/homework/homework.js
+++ 04-EstructuraDeDatos-I/homework/homework.js
@@ -14,9 +14,19 @@ Secuencia:  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
 Como ejercicio adicional y completamente opcional, al terminar de resolver este problema pueden intentar definir funciones que logren los mismos resultados pero de manera iterativa.
 */
 
-function nFactorial(n) {}
+function nFactorial(n) {
+  if (n === 1) {
+    return 1
+  }
+  return(n * nFactorial(n-1))
+}
 
-function nFibonacci(n) {}
+function nFibonacci(n) {
+  if (n === 0) return 0
+  if (n === 1) return 1
+  
+  return (nFibonacci(n-1)+ nFibonacci(n-2))
+}
 
 /*
 Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde el primer elemento que ingresa es el primero que se quita. Definir los siguientes métodos:
@@ -27,7 +37,22 @@ Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde e
 Pueden utilizar class o función constructora.
 */
 
-function Queue() {}
+function Queue() {
+  this.arr = []
+}
+
+Queue.prototype.enqueue = function (valor) {
+  this.arr.push(valor)
+}
+
+Queue.prototype.dequeue = function (valor) {
+  if(this.arr.length === 0) return undefined
+  return this.arr.shift()
+}
+
+Queue.prototype.size = function () {
+  return this.arr.length
+}
 
 /*⚠️ No modificar nada debajo de esta línea ⚠️*/
 module.exports = {
